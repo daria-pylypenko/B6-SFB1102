35a91501da09361d0a16d89ae9dbff4d8f276210
Daria <pylypenko.daria@gmail.com>
2021-10-30 01:37:18 +0200

    Modified classifier.py
    - Enabled forwarding some config options
    - Enabled automatic calculation of train-test boundary
    - Enabled calculation of precision, recall and f1 for non-binary classification

--- infodens/classifier/classifier.py
+++ infodens/classifier/classifier.py
@@ -38,41 +38,62 @@ class Classifier(object):
 
     classifierName = ''
     
-    def __init__(self, dataX, datay, threads=1, nCrossValid=2):
+    def __init__(self, dataX, datay, threads=1, nCrossValid=1,
+                             modelInput="", modelOutput = "",
+                            train_size=0, val_size=0, random_state=None):
         self.X = dataX
         self.y = datay
         self.threadCount = threads
         self.n_foldCV = nCrossValid
+        self.modelInput = modelInput
+        self.modelOutput = modelOutput
+        self.train_size = train_size
+        self.val_size = val_size
+        self.random_state = random_state
 
     def shuffle(self):
         self.X, self.y = sklearn.utils.shuffle(self.X, self.y)
         
+
     def splitTrainTest(self):
         #self.Xtrain, self.Xtest, self.ytrain, self.ytest = model_selection.train_test_split(self.X, self.y,
         #                                                                                    test_size=self.splitPercent,
         #                                                                                    random_state=0)
 
+        # try random vectors
+        #self.X = np.random.rand(self.X.shape[0], self.X.shape[1])
 
         # Use a predefined train-test boundary (train includes val too)
         # len_train + len_val
-        train_test_boundary = 35846
+        train_test_boundary = self.train_size + self.val_size
+        #train_test_boundary = 35846
+        #train_test_boundary = 35847
+        #train_test_boundary = 107541
         self.Xtrain = self.X[:train_test_boundary]
         self.Xtest = self.X[train_test_boundary:]
         self.ytrain = self.y[:train_test_boundary]
         self.ytest = self.y[train_test_boundary:]
+
+        print("Unique labels")
+        print(len(set(self.ytest)))
 
     def predict(self):
         return self.model.predict(self.Xtest)
 
     def evaluate(self):
         y_pred = self.predict()
-        return accuracy_score(self.ytest, y_pred), precision_score(self.ytest, y_pred),\
-               recall_score(self.ytest, y_pred), f1_score(self.ytest, y_pred)
+        if len(set(self.ytest)) > 2:
+            average = "micro"
+        else:
+            average = "binary"
+        return accuracy_score(self.ytest, y_pred), precision_score(self.ytest, y_pred, average=average),\
+               recall_score(self.ytest, y_pred, average=average), f1_score(self.ytest, y_pred, average=average)
         
     def runClassifier(self):
         """ Run the provided classifier."""
         acc = []; pre = []; rec = []; fsc = []
 
+        print("Number of CV folds:", self.n_foldCV)
         for i in range(self.n_foldCV):
             #self.shuffle()
             self.splitTrainTest()
